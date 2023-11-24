820f772348f49aa4533a4db6e7627eea9d417b2a
Shun2439 <shunta.kobayashi24@gmail.com>
2023-11-24 16:13:21 +0900

    :white_check_mark:/wez config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,37 +1,38 @@
 local wezterm = require 'wezterm'
 
 local config = {
-    --windowの枠を消す
+    -- windowの枠を消す
     window_decorations = "RESIZE",
 
     -- 透過度
-    --window_background_opacity = 0.95,
+    window_background_opacity = 0.8,
 
-    hide_tab_bar_if_only_one_tab = true,
+    hide_tab_bar_if_only_one_tab = true
 }
 
 -- 使うフォントを指定する
 config.font = wezterm.font 'Hack'
 
-config.color_scheme = 'nord'
+-- config.color_scheme = 'nord'
 
---config.window_background_image = 'C:\\Users\\shunt\\workdir\\config\\WezTerm\\I.png'
+-- config.window_background_image = 'C:\\Users\\shunt\\workdir\\config\\WezTerm\\I.png'
 
 config.window_background_image_hsb = {
-  -- Darken the background image by reducing it to 1/3rd
-  brightness = 0.3,
+    -- Darken the background image by reducing it to 1/3rd
+    brightness = 0.3,
 
-  -- You can adjust the hue by scaling its value.
-  -- a multiplier of 1.0 leaves the value unchanged.
-  hue = 1.0,
+    -- You can adjust the hue by scaling its value.
+    -- a multiplier of 1.0 leaves the value unchanged.
+    hue = 1.0,
 
-  -- You can adjust the saturation also.
-  saturation = 1.0,
+    -- You can adjust the saturation also.
+    saturation = 1.0
 }
 
 -- weztermの起動時に呼び出すシェルを指定する
---wsl
-config.default_prog = {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_1.2.5.0_x64__8wekyb3d8bbwe\\wsl.exe'}
+-- wsl
+config.default_prog =
+    {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_2.0.9.0_x64__8wekyb3d8bbwe\\wsl.exe'}
 
 -- powershell
 -- config.default_prog = {'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '-l'} 
