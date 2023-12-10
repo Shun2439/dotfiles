0df292aff45442ae67de80d512b66598f9b3a5d1
Shun2439 <shunta.kobayashi24@gmail.com>
2023-12-11 07:34:50 +0900

    :art:

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,40 +1,26 @@
 local wezterm = require 'wezterm'
 
 local config = {
-    -- windowの枠を消す
-    window_decorations = "RESIZE",
+	-- windowの枠を消す
+	window_decorations = "RESIZE",
 
-    -- 透過度
-    window_background_opacity = 0.8,
+	window_background_opacity = 0.85,
 
-    hide_tab_bar_if_only_one_tab = true
+	hide_tab_bar_if_only_one_tab = true,
+
+	color_scheme = 'nord',
+
+	-- 使うフォントを指定する
+	font = wezterm.font 'Hack',
+
+	-- weztermの起動時に呼び出すシェルを指定する
+	default_prog = {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_2.0.9.0_x64__8wekyb3d8bbwe\\wsl.exe'}, --wsl
+	-- default_prog = {'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '-l'}, --powershell
+
+	colors = {
+		cursor_bg = 'silver',
+		cursor_fg = 'black'
+	},
 }
-
--- 使うフォントを指定する
-config.font = wezterm.font 'Hack'
-
--- config.color_scheme = 'nord'
-
--- config.window_background_image = 'C:\\Users\\shunt\\workdir\\config\\WezTerm\\I.png'
-
-config.window_background_image_hsb = {
-    -- Darken the background image by reducing it to 1/3rd
-    brightness = 0.3,
-
-    -- You can adjust the hue by scaling its value.
-    -- a multiplier of 1.0 leaves the value unchanged.
-    hue = 1.0,
-
-    -- You can adjust the saturation also.
-    saturation = 1.0
-}
-
--- weztermの起動時に呼び出すシェルを指定する
--- wsl
-config.default_prog =
-    {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_2.0.9.0_x64__8wekyb3d8bbwe\\wsl.exe'}
-
--- powershell
--- config.default_prog = {'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '-l'} 
 
 return config
