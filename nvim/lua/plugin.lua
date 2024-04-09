5a0ba88fd2732f704f7f0c74fd18ed49658427c8
Shun2439 <shunta.kobayashi24@gmail.com>
2024-04-10 06:53:40 +0900

    :white_check_mark: flutter-tools

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,17 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{"neoclide/coc.nvim", branch = 'release' }
-	})
+	{
+		"neoclide/coc.nvim", branch = 'release'
+	},
+	{
+		'akinsho/flutter-tools.nvim',
+		lazy = false,
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'stevearc/dressing.nvim', -- optional for vim.ui.select
+		},
+		config = true,
+	}
+})
 
