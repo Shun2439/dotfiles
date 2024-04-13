4a21f761dae5c1135628371814896ab72e2bfe1b
Shun2439 <shunta.kobayashi24@gmail.com>
2024-04-13 23:23:04 +0900

    :white_check_mark: powershell path
    :construction:
    call profile function

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,11 +9,18 @@ local config = {
 	hide_tab_bar_if_only_one_tab = true,
 
 	-- 使うフォントを指定する
-	font = wezterm.font 'Hack NF',
+	font = wezterm.font 'Moralerspace Radon HWNF',
 
 	-- weztermの起動時に呼び出すシェルを指定する
-	default_prog = {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_2.0.9.0_x64__8wekyb3d8bbwe\\wsl.exe'}, --wsl
-	-- default_prog = {'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '-l'}, --powershell
+	-- default_prog = {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_3.0.9.0_x64__8wekyb3d8bbwe\\wsl.exe'},
+	default_prog = {'C:\\soft\\PowerShell\\7\\pwsh.exe', '-l'}, 
+	-- keys = {
+	-- 	{
+	-- 		key = '1',
+	-- 		mods = 'CTRL|SHIFT',
+	-- 		action = {'C:\\soft\\PowerShell\\7\\pwsh.exe', '-l'},
+	-- 	},
+	-- }
 }
 
 return config
