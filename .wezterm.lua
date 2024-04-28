c6ec2b178ec6bef65584fa7aa782df05bb6a1d6d
Shun2439 <shunta.kobayashi24@gmail.com>
2024-04-28 10:18:15 +0900

    ✅ shortcuts - ctrl + shift + 1(!) -> posh - ctrl + shift + 2(") -> wsl 🔥 default shell options

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,26 +1,50 @@
 local wezterm = require 'wezterm'
 
+-- 新規タブでPowerShell 7を開く
+wezterm.on('SpawnNewTabOnPwsh', function(window, pane)
+    window:perform_action(wezterm.action {
+        SpawnCommandInNewTab = {
+            args = {"pwsh.exe"}
+        }
+    }, pane)
+end)
+
+-- 新規タブでWslを開く
+wezterm.on('SpawnNewTabOnWsl', function(window, pane)
+    window:perform_action(wezterm.action {
+        SpawnCommandInNewTab = {
+            args = {"wsl.exe"} -- WSLのホームディレクトリを指定
+        }
+    }, pane)
+end)
+
 local config = {
-	-- windowの枠を消す
-	window_decorations = "RESIZE",
+    -- windowの枠を消す
+    window_decorations = "RESIZE",
 
-	window_background_opacity = 0.8,
+    window_background_opacity = 0.8,
 
-	hide_tab_bar_if_only_one_tab = true,
+    hide_tab_bar_if_only_one_tab = true,
 
-	-- 使うフォントを指定する
-	font = wezterm.font 'Moralerspace Radon HWNF',
+    -- 使うフォントを指定する
+    font = wezterm.font 'Moralerspace Radon HWNF',
 
-	-- weztermの起動時に呼び出すシェルを指定する
-	-- default_prog = {'C:\\Program Files\\WindowsApps\\MicrosoftCorporationII.WindowsSubsystemForLinux_3.0.9.0_x64__8wekyb3d8bbwe\\wsl.exe'},
-	default_prog = {'C:\\Program Files\\PowerShell\\7\\pwsh.exe'}, 
-	-- keys = {
-	-- 	{
-	-- 		key = '1',
-	-- 		mods = 'CTRL|SHIFT',
-	-- 		action = {'C:\\soft\\PowerShell\\7\\pwsh.exe', '-l'},
-	-- 	},
-	-- }
+    -- weztermの起動時に呼び出すシェルを指定する
+    default_prog = {'pwsh.exe'},
+
+    keys = {{
+        key = '!',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action({
+            EmitEvent = "SpawnNewTabOnPwsh"
+        })
+    }, {
+        key = '"',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action({
+            EmitEvent = "SpawnNewTabOnWsl"
+        })
+    }}
 }
 
 return config
