ecd34e19a310499ae7863c44383003f1ecda67af
Shun2439 <shunta.kobayashi24@gmail.com>
2024-05-09 10:40:54 +0900

    ✅ load dictionary

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,19 +1,21 @@
-local opts = {
-    noremap = true,
-    silent = true
-}
-local term_opts = {
-    silent = true
-}
-local keymap = vim.api.nvim_set_keymap
+function join_paths(...)
+  local path_sep = on_windows and "\\" or "/"
+  local result = table.concat({ ... }, path_sep)
+  return result
+end
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local data_path = vim.fn.stdpath("data")
+
+local lazypath = data_path .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system(
         {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
          lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
+
+local dictionary_source_path = join_paths(data_path, "SKK-JISYO.L")
+local my_dictionary_path = join_paths(data_path, "SKK-JISYO.MY")
 
 require("lazy").setup({{
     "neoclide/coc.nvim",
@@ -25,20 +27,32 @@ require("lazy").setup({{
     },
     config = true
 }, {
-    "vim-denops/denops.vim",
-    lazy = false
-}, {
     "vim-skk/skkeleton",
-    lazy = false
+    dependencies = {"vim-denops/denops.vim"},
+    lazy = false,
+    config = function()
+        vim.fn['skkeleton#config']({
+            globalDictionaries = { dictionary_source_path },
+            userDictionary = my_dictionary_path,
+        })
+        vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-toggle)")
+        vim.keymap.set("c", "<C-j>", "<Plug>(skkeleton-toggle)")
+        vim.keymap.set("t", "<C-j>", "<Plug>(skkeleton-toggle)")
+    end
+}, {
+    "delphinus/skkeleton_indicator.nvim",
+    config = function()
+        vim.keymap.set("n", "[j", "<Plug>(jumpcursor-jump)")
+    end
+}, {
+    "skanehira/jumpcursor.vim",
+    config = function()
+        require('skkeleton_indicator').setup {}
+    end
+}, {
+    "EdenEast/nightfox.nvim",
+    config = function()
+        vim.cmd("colorscheme terafox")
+    end
 }})
-
----------------
--- skkeleton --
----------------
-vim.fn['skkeleton#config']({
-    globalDictionaries = "~/.config/SKK-JISYO.L"
-})
-keymap("i", "<C-j>", "<Plug>(skkeleton-toggle)", opts)
-keymap("c", "<C-j>", "<Plug>(skkeleton-toggle)", opts)
-keymap("t", "<C-j>", "<Plug>(skkeleton-toggle)", opts)
 
