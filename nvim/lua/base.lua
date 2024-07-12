ae8fbfc1df97d6b4345232dde6dafaee71ea961a
Shun2439 <shunta.kobayashi24@gmail.com>
2024-07-12 22:26:27 +0900

    ✅ customize filer

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -9,3 +9,14 @@ vim.o.sw = 2
 vim.o.ts = 2
 vim.o.smartindent = true
 
+-- ファイルツリーの表示形式、1にするとls -laのような表示になります
+vim.g.netrw_liststyle=3
+-- ヘッダを非表示にする
+vim.g.netrw_banner=0
+-- サイズを(K,M,G)で表示する
+vim.g.netrw_sizestyle="H"
+-- 日付フォーマットを yyyy/mm/dd(曜日) hh:mm:ss で表示する
+vim.g.netrw_timefmt="%Y/%m/%d(%a) %H:%M:%S"
+-- プレビューウィンドウを垂直分割で表示する
+vim.g.netrw_preview=1
+
