b349c6f0b893f421d69f5ae7855d7b6996abcd32
Shun2439 <shunta.kobayashi24@gmail.com>
2025-01-27 21:49:19 +0900

    🙈 fix ignore
    ✅ add shell configurations
    
    - fish
    - nushell

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+source "$HOME/.cargo/env.fish"
+
+export GPG_TTY=$(tty)
+
