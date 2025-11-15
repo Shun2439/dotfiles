ff464508fb172d929bb41d0b0069fcc245fa3424
Shun2439 <shunta.kobayashi24@gmail.com>
2025-11-16 01:31:14 +0900

    chore: yazi

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,13 @@ Set-Alias -Name ls -Value eza
 Set-Alias -Name e -Value emacsclient
 
 Set-PSReadLineOption -EditMode Emacs
+
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
