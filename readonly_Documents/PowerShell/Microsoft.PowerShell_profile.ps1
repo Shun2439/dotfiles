394fdf0b08814f2587c3fe59c30976d58e19e389
Shun2439 <shunta.kobayashi24@gmail.com>
2025-11-25 12:38:22 +0900

    chore: add zoxide flag

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 Invoke-Expression (&starship init powershell)
 
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+
 function Get-ChildItem {
     eza --icons $args
 }
@@ -17,3 +19,12 @@ function y {
     }
     Remove-Item -Path $tmp
 }
+
+# IntelliShell
+# $env:INTELLI_HOME = "C:\Users\$Env:USERNAME\AppData\Roaming\IntelliShell\Intelli-Shell\data" # Is it correct?
+# $env:INTELLI_SEARCH_HOTKEY = 'Ctrl+Spacebar'
+# $env:INTELLI_VARIABLE_HOTKEY = 'Ctrl+l'
+# $env:INTELLI_BOOKMARK_HOTKEY = 'Ctrl+b'
+# $env:INTELLI_FIX_HOTKEY = 'Ctrl+x'
+# Set-Alias -Name 'is' -Value 'intelli-shell'
+# intelli-shell.exe init powershell | Out-String | Invoke-Expression
