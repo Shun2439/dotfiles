333798ae71cc78a9779047aa446ea9c088e22d53
Shun2439 <shunta.kobayashi24@gmail.com>
2025-12-30 03:49:02 +0900

    feat: update tab bar

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,6 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require('wezterm')
 local mux = wezterm.mux
 local act = wezterm.action
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 
 wezterm.on("gui-startup", function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
@@ -11,11 +14,11 @@ wezterm.on('update-right-status', function(window, pane)
     window:set_right_status(window:active_workspace())
 end)
 
--- mmタブでcmdを開く
+-- 新規タブでcmdを開く
 wezterm.on('SpawnNewTabOnCmd', function(window, pane)
     window:perform_action(wezterm.action {
         SpawnCommandInNewTab = {
-            args = { "cmd.exe" } -- WSLのホームディレクトリを指定
+            args = {"cmd.exe"}
         }
     }, pane)
 end)
@@ -24,18 +27,43 @@ end)
 wezterm.on('SpawnNewTabOnPwsh', function(window, pane)
     window:perform_action(wezterm.action {
         SpawnCommandInNewTab = {
-            args = { "pwsh.exe" }
+            args = {"pwsh.exe"}
         }
     }, pane)
 end)
 
--- 新規タブでWslを開く
+-- 新規タブでWSLを開く
 wezterm.on('SpawnNewTabOnWsl', function(window, pane)
     window:perform_action(wezterm.action {
         SpawnCommandInNewTab = {
-            args = { "wsl.exe" } -- WSLのホームディレクトリを指定
+            args = {"wsl.exe"}
         }
     }, pane)
+end)
+
+function tab_title(tab_info)
+    local title = tab_info.tab_title
+    if title and #title > 0 then
+        return title
+    end
+
+    return tab_info.active_pane.title
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local title = "   " .. wezterm.truncate_right(tab_title(tab), max_width - 1) .. "   "
+
+    if tab.is_active then
+        return {{
+            Text = SOLID_LEFT_ARROW .. title .. SOLID_RIGHT_ARROW
+        }}
+    end
+    if tab.is_last_active then
+        return {{
+            Text = SOLID_LEFT_ARROW .. title .. '*'
+        }}
+    end
+    return title
 end)
 
 -- <https://github.com/wez/wezterm/issues/4429#issuecomment-1774827062>
@@ -59,69 +87,66 @@ wezterm.on('toggle-colorscheme', function(window, pane)
 end)
 
 local config = {
-    front_end = "WebGpu",
+    front_end = "WebGpu", -- Windowsでの描画不良対策
+
     automatically_reload_config = true,
+
     -- windowの枠を消す
     window_decorations = "RESIZE",
 
     -- <https://github.com/ryoppippi/dotfiles/blob/main/wezterm/tab_bar.lua>
     -- タブバーの非表示
     hide_tab_bar_if_only_one_tab = true,
+
     -- 新規タブの`+`を非表示
     show_new_tab_button_in_tab_bar = false,
+
     -- どちらがいいか悩む
     use_fancy_tab_bar = false,
 
+    -- タブバーの透過
+    -- only for MacOS?
     window_frame = {
-        inactive_titlebar_bg = "none",
         active_titlebar_bg = "none",
+        inactive_titlebar_bg = "none"
     },
 
-    -- TODO change tab colorscheme
+    -- タブバーを背景色に合わせる
+    window_background_gradient = {
+        colors = {"#000000"}
+    },
+
+    -- for catppuccin
     colors = {
         tab_bar = {
-            --         background = "#1b1f2f",
-            inactive_tab_edge = "none",
+            background = '#11111b', -- Mocha Crust
+            inactive_tab_edge = '#11111b',
 
-            --         active_tab = {
-            --             bg_color = "#444b71",
-            --             fg_color = "#c6c8d1",
-            --             intensity = "Normal",
-            --             underline = "None",
-            --             italic = false,
-            --             strikethrough = false
-        },
+            active_tab = {
+                bg_color = '#b4befe', -- Mocha Lavender
+                fg_color = '#1e1e2e' -- Base
+            },
 
-        --         inactive_tab = {
-        --             bg_color = "#282d3e",
-        --             fg_color = "#c6c8d1",
-        --             intensity = "Normal",
-        --             underline = "None",
-        --             italic = false,
-        --             strikethrough = false
-        --         },
+            inactive_tab = {
+                bg_color = '#181825', -- Mantle
+                fg_color = '#a6adc8' -- Subtext 0
+            },
 
-        --         inactive_tab_hover = {
-        --             bg_color = "#1b1f2f",
-        --             fg_color = "#c6c8d1",
-        --             intensity = "Normal",
-        --             underline = "None",
-        --             italic = true,
-        --             strikethrough = false
-        --         },
+            inactive_tab_hover = {
+                bg_color = '#313244', -- Surface 0
+                fg_color = '#cdd6f4' -- Text
+            },
 
-        --         new_tab = {
-        --             bg_color = "#1b1f2f",
-        --             fg_color = "#c6c8d1",
-        --             italic = false
-        --         },
+            new_tab = {
+                bg_color = '#11111b',
+                fg_color = '#a6adc8'
+            },
 
-        --         new_tab_hover = {
-        --             bg_color = "#444b71",
-        --             fg_color = "#c6c8d1",
-        --             italic = false
-        --         }
-        --     }
+            new_tab_hover = {
+                bg_color = '#313244',
+                fg_color = '#cdd6f4'
+            }
+        }
     },
 
     window_padding = {
@@ -135,31 +160,14 @@ local config = {
     color_scheme = 'Catppuccin Mocha',
     -- color_scheme = 'Halloween Pumpkin',
 
+    -- for 10/31
     color_schemes = {
         ['Halloween Pumpkin'] = {
             background = '#1a0d00',
             foreground = '#e0a050',
 
-            ansi = {
-                '#220d00',
-                '#d35400',
-                '#7f8c8d',
-                '#f39c12',
-                '#6a5acd',
-                '#8e44ad',
-                '#bbbbbb',
-                '#ecf0f1',
-            },
-            brights = {
-                '#555555',
-                '#e74c3c',
-                '#5d8f6d',
-                '#f1c40f',
-                '#8458b3',
-                '#9b59b6',
-                '#a9d5e7',
-                '#ffffff',
-            },
+            ansi = {'#220d00', '#d35400', '#7f8c8d', '#f39c12', '#6a5acd', '#8e44ad', '#bbbbbb', '#ecf0f1'},
+            brights = {'#555555', '#e74c3c', '#5d8f6d', '#f1c40f', '#8458b3', '#9b59b6', '#a9d5e7', '#ffffff'},
 
             selection_bg = '#a0522d',
             selection_fg = '#ffffff',
@@ -173,18 +181,18 @@ local config = {
                 active_tab = {
                     bg_color = '#d35400',
                     fg_color = '#1a0d00',
-                    intensity = 'Normal',
+                    intensity = 'Normal'
                 },
                 inactive_tab = {
                     bg_color = '#331a00',
-                    fg_color = '#888888',
+                    fg_color = '#888888'
                 },
                 new_tab = {
                     bg_color = '#280f00',
-                    fg_color = '#888888',
-                },
-            },
-        },
+                    fg_color = '#888888'
+                }
+            }
+        }
     },
 
     -- <https://wezterm.org/config/appearance.html#window-background-image>
@@ -196,9 +204,9 @@ local config = {
     font = wezterm.font 'Moralerspace Radon HWNF',
 
     -- weztermの起動時に呼び出すシェルを指定する
-    default_prog = { 'pwsh.exe' },
+    default_prog = {'pwsh.exe'},
 
-    keys = { {
+    keys = {{
         key = '!',
         mods = 'CTRL|SHIFT',
         action = wezterm.action({
@@ -229,19 +237,19 @@ local config = {
             name = 'default'
         }
     }, -- Create a new workspace with a random name and switch to it
-        {
-            key = 'n',
-            mods = 'ALT',
-            action = act.SwitchToWorkspace
-        }, -- Show the launcher in fuzzy selection mode and have it list all workspaces
-        -- and allow activating one.
-        {
-            key = '9',
-            mods = 'ALT',
-            action = act.ShowLauncherArgs {
-                flags = 'FUZZY|WORKSPACES'
-            }
-        }, {
+    {
+        key = 'n',
+        mods = 'ALT',
+        action = act.SwitchToWorkspace
+    }, -- Show the launcher in fuzzy selection mode and have it list all workspaces
+    -- and allow activating one.
+    {
+        key = '9',
+        mods = 'ALT',
+        action = act.ShowLauncherArgs {
+            flags = 'FUZZY|WORKSPACES'
+        }
+    }, {
         key = ')',
         mods = 'ALT|SHIFT',
         action = wezterm.action.ShowTabNavigator
@@ -253,9 +261,9 @@ local config = {
         key = 'K',
         mods = 'ALT|SHIFT',
         action = act.SwitchWorkspaceRelative(-1)
-    } },
+    }},
 
-    ssh_domains = { {
+    ssh_domains = {{
         -- This name identifies the domain
         name = 'my.server',
         -- The hostname or address to connect to. Will be used to match settings
@@ -263,7 +271,7 @@ local config = {
         remote_address = '192.168.1.1',
         -- The username to use on the remote host
         username = 'shun'
-    } }
+    }}
 }
 
 -- activate tab
